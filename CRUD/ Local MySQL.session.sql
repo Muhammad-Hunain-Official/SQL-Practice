5ccedf8b8d4a4ce940403de92be2d5caa5c30f7c
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-07 00:49:18 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,7 +1,5 @@
-use startersql;
-SELECT DATABASE();
-
-SHOW TABLES;
-
-
-SELECT age,city FROM startersql.students;
+USE startersql;
+DELETE FROM startersql.students
+WHERE age < 19;
+SELECT *
+FROM startersql.students;
