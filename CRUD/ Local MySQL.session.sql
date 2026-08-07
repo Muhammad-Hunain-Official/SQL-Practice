0e58f1f13a803ec8e4eb50add334bbf4ae1b92cb
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-07 15:17:12 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,7 +1,3 @@
 USE startersql;
-INSERT INTO students
-(student_id, first_name, last_name, age, gender, city, department, semester, cgpa, admission_date)
-VALUES
-(111, 'Ali', 'Khan', 30, 'Male', 'Lahore', 'Computer Science', 3, 3.45, '2024-09-01');
-SELECT student_id ,age  FROM startersql.students
-
+-- SELECT MIN(age) As min_age , MAX(age) AS max_age FROM startersql.students
+SELECT AVG(cgpa) AS avg_cgpa FROM startersql.students
