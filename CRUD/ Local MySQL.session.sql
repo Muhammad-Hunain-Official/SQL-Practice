6cc69f41f7d92824853067fdf1564d7b77768fd8
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-07 17:18:05 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -2,4 +2,17 @@ USE startersql;
 -- SELECT MIN(age) As min_age , MAX(age) AS max_age FROM startersql.students
 -- SELECT gender, AVG(cgpa) AS avg_cgpa FROM startersql.students GROUP BY gender
 -- select first_name, LENGTH(first_name) AS name_len FROM startersql.students
-SELECT first_name,gender, IF(gender = 'Male', 'Yes', 'No') as Eligibe FROM startersql.students
+-- SELECT first_name,gender, IF(gender = 'Male', 'Yes', 'No') as Eligibe FROM startersql.students
+SET autocommit = 0;
+-- DELETE FROM startersql.students
+-- WHERE student_id = 102;
+SELECT * FROM startersql.students
+
+-- SELECT *
+-- FROM startersql.students
+-- WHERE student_id = 102;
+ROLLBACK;
+SELECT *
+FROM startersql.students
+WHERE student_id = 102;
+COMMIT;
