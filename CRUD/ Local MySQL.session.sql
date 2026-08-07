c52e8ad1d87caaf4b9552d42b5019f886415ae79
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-07 15:33:32 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,4 +1,5 @@
 USE startersql;
 -- SELECT MIN(age) As min_age , MAX(age) AS max_age FROM startersql.students
 -- SELECT gender, AVG(cgpa) AS avg_cgpa FROM startersql.students GROUP BY gender
-select first_name, LENGTH(first_name) AS name_len FROM startersql.students
+-- select first_name, LENGTH(first_name) AS name_len FROM startersql.students
+SELECT first_name,gender, IF(gender = 'Male', 'Yes', 'No') as Eligibe FROM startersql.students
