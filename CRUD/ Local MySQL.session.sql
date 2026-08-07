142651073c06705658ea4a3e764a64d51d4cafc4
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-07 15:25:06 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,3 +1,4 @@
 USE startersql;
 -- SELECT MIN(age) As min_age , MAX(age) AS max_age FROM startersql.students
-SELECT AVG(cgpa) AS avg_cgpa FROM startersql.students
+-- SELECT gender, AVG(cgpa) AS avg_cgpa FROM startersql.students GROUP BY gender
+select first_name, LENGTH(first_name) AS name_len FROM startersql.students
