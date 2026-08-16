fad50df200417838a2df6b1cca465f4d2ed9ec20
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-16 15:31:49 +0500

    working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,1 +1,9 @@
 USE startersql;
+ALTER TABLE students
+ADD COLUMN refred_by_age INT;
+
+
+
+UPDATE students SET refred_by_age = 1 WHERE age IN (18 , 19 , 20);
+UPDATE students SET refred_by_age = 0 WHERE age = 21
+SELECT * FROM startersql.students;
--- CRUD/04_admin_user.sql
+++ CRUD/04_admin_user.sql
@@ -1,4 +1,5 @@
 USE startersql;
+
 CREATE TABLE admin_users (
     admin_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
@@ -6,13 +7,29 @@ CREATE TABLE admin_users (
     gender VARCHAR(10),
     salary DECIMAL(10,2)
 );
+
 INSERT INTO admin_users (name, email_id, gender, salary)
 VALUES
-('Ali Khan', 'ali@gmail.com', 'Male', 75000),
+('Ali', 'kk@gmail.com', 'Male', 75000),
 ('Ayesha Ahmed', 'ayesha@gmail.com', 'Female', 85000),
 ('Usman Ali', 'usman@gmail.com', 'Male', 65000),
 ('Sara Hassan', 'sara@gmail.com', 'Female', 90000);
+-- DELETE FROM admin_users
+-- WHERE admin_id = 1;
+INSERT INTO admin_users (name, email_id, gender, salary)
+VALUES
+('Ali', 'hamza@gmail.com', 'Male', 70000);
 
-SELECT first_name FROM startersql.students
+SELECT first_name
+FROM startersql.students
+
 UNION
-SELECT name FROM startersql.admin_users
+
+SELECT name
+FROM startersql.admin_users;
+ 
+ALTER TABLE admin_users AUTO_INCREMENT = 5;
+INSERT INTO admin_users (name, email_id, gender, salary)
+VALUES ('alina', 'sa@gmail.com', 'Male', 70000);
+SELECT * FROM startersql.admin_users 
+SELECT * FROM startersql.students
