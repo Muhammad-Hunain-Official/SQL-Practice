2c67f78f522c6647ee00ed371a32181e77925c76
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-17 18:00:54 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,33 +1,37 @@
-USE startersql;
-ALTER TABLE students
-ADD COLUMN refred_by_age INT;
+-- USE startersql;
+-- ALTER TABLE students
+-- ADD COLUMN refred_by_age INT;
 
 
 
-UPDATE students SET refred_by_age = 1 WHERE age IN (18 , 19 , 20 , 22);
-UPDATE students SET refred_by_age = 0 WHERE age = 21
-SELECT * FROM startersql.students;
-SELECT 
-a.student_id,
-a.first_name AS Student_name,
-b.first_name AS ref_by_name
-FROM students a INNER JOIN students b ON a.refred_by_age = b.student_id;
+-- UPDATE students SET refred_by_age = 1 WHERE age IN (18 , 19 , 20 , 22);
+-- UPDATE students SET refred_by_age = 0 WHERE age = 21
+-- SELECT * FROM startersql.students;
+-- SELECT 
+-- a.student_id,
+-- a.first_name AS Student_name,
+-- b.first_name AS ref_by_name
+-- FROM students a INNER JOIN students b ON a.refred_by_age = b.student_id;
 
-SELECT DISTINCT refred_by_age
-FROM students;
+-- SELECT DISTINCT refred_by_age
+-- FROM students;
 
-SELECT student_id
-FROM students;
+-- SELECT student_id
+-- FROM students;
 
-SELECT
-    a.student_id,
-    a.first_name,
-    a.refred_by_age,
-    b.student_id,
-    b.first_name
-FROM students a
-LEFT JOIN students b
-ON a.refred_by_age = b.student_id;
+-- SELECT
+--     a.student_id,
+--     a.first_name,
+--     a.refred_by_age,
+--     b.student_id,
+--     b.first_name
+-- FROM students a
+-- LEFT JOIN students b
+-- ON a.refred_by_age = b.student_id;
 
 
-SELECT *  FROM startersql.students
+-- SELECT *  FROM startersql.students
+
+
+CREATE VIEW rich_students AS SELECT * FROM students WHERE age > 18;
+SELECT * FROM rich_students
