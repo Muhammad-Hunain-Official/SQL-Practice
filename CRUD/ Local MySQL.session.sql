bfd7dba4d0a4ba086d66d036fbc865252cb55723
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-19 01:13:10 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -1,4 +1,15 @@
 USE startersql;
-SELECT AVG(cgpa) FROM startersql.students;
+-- SELECT AVG(cgpa) FROM startersql.students;
  
-SELECT * FROM startersql.students WHERE cgpa > (SELECT AVG(cgpa) FROM startersql.students); 
+-- SELECT * FROM startersql.students WHERE cgpa > (SELECT AVG(cgpa) FROM startersql.students); 
+
+
+SELECT student_id, first_name
+FROM students
+WHERE age IN (
+    SELECT age
+    FROM students
+    WHERE cgpa > 3.5
+);
+SELECT student_id, first_name, cgpa, refred_by_age
+FROM startersql.students;
