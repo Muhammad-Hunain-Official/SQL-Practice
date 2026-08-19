5344d3b00e285defdf789bbfcaeecc6df664f7a0
Muhammad-Hunain-Official <hunainejaz200@gmail.com>
2026-08-19 12:56:21 +0500

    Working

--- CRUD/ Local MySQL.session.sql
+++ CRUD/ Local MySQL.session.sql
@@ -4,12 +4,13 @@ USE startersql;
 -- SELECT * FROM startersql.students WHERE cgpa > (SELECT AVG(cgpa) FROM startersql.students); 
 
 
-SELECT student_id, first_name
-FROM students
-WHERE age IN (
-    SELECT age
-    FROM students
-    WHERE cgpa > 3.5
-);
-SELECT student_id, first_name, cgpa, refred_by_age
-FROM startersql.students;
+-- SELECT student_id, first_name
+-- FROM students
+-- WHERE age IN (
+--     SELECT age
+--     FROM students
+--     WHERE cgpa > 3.5
+-- );
+-- SELECT student_id, first_name, cgpa, refred_by_age
+-- FROM startersql.students;
+SELECT gender , AVG(cgpa), COUNT(*) as 'Avg' FROM startersql.students GROUP BY gender;
